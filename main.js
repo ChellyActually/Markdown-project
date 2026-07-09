a4ddadc84104eac0c547b3d28db655ea651ebc2d
1302yogeswari-netizen <1302yogeswari@gmail.com>
2026-07-09 12:02:29 +0530

    updated

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const input = document.getElementById("text");
+const output = document.getElementById("preview");
+function updatePreview()
+{
+    const text = input.value;
+    const html = mdToHtml(text);
+    output.innerHtml = html;
+}
+
